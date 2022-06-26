863b061977ecb2c78301d1703475ffd32af330b1
WannTonn <895522268@qq.com>
2022-06-26 21:33:40 +0800

    feat: update blogs

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 # git add to git push
 git add .
-
+echo "git add"
 git commit -m "feat: update blogs"
-
+echo "git commit"
 sleep 5s
-
+echo "sleep 5s for commit"
 git push origin master
+echo "git push"
